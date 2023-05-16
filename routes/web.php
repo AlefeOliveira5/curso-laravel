ed7fc7e132aafa2b1c786dcec225d101431870a7
AlefeOliveira5 <lefeoliveira@gmail.com>
2023-05-16 00:14:22 -0300

    Subindo nova versão

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\ProdutoController;
 /*
 |--------------------------------------------------------------------------
 | Web Routes
@@ -13,6 +13,32 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/',[ProdutoController::class, 'index'])->name('produto.index');
+Route::get('/produto/{id?}',[ProdutoController::class, 'show'])->name('produto.show');
+
+Route::get('/empresa', function () {
+    return view('site1/empresa');
+});
+
+Route::any('/any', function () {
+    return "PERMITE TODO TIPO DE ACESSO HTTP";
+});
+
+Route::match(['put', 'delete'], '/match', function () {
+    return "PERMITE APENAS ACESSOS DEFINIDOS";
+});
+
+Route::get('/produto/{id}/{cat}', function ($id, $cat) {
+    return "o id do produto é:".$id."<br>"."A categoria é:".$cat;
+});
+
+Route::redirect('/sobre', '/empresa');
+Route::view('/empresa', 'site1/empresa');
+
+Route::get('/news', function(){
+    return view('news');
+})->name('noticias');
+
+Route::get('/novidades', function(){
+    return redirect()->route('noticias');
 });
